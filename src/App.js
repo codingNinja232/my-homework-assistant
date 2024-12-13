45029dee1609f91dd2f92bd7ee3e2aa99a7b8880
codingNinja232 <coding.ninja232@gmail.com>
2024-12-13 18:06:17 +0530

    Added Science as well

--- src/App.js
+++ src/App.js
@@ -17,14 +17,7 @@ const App = () => {
   const hardcodedQuestions = [
     { question: "Please wait while the questions are being loaded", options: ["", "", "", ""], answer: 5 },
     { question: "What is the capital of France?", options: ["Berlin", "London", "Paris", "Rome"], answer: 2 },
-    { question: "What color is the sky?", options: ["Blue", "Green", "Red", "Yellow"], answer: 0 },
-    { question: "What is 5 x 3?", options: ["15", "20", "10", "25"], answer: 0 },
-    { question: "Which is a mammal?", options: ["Shark", "Frog", "Dog", "Eagle"], answer: 2 },
-    { question: "What is the boiling point of water?", options: ["90°C", "100°C", "80°C", "110°C"], answer: 1 },
-    { question: "What planet do we live on?", options: ["Mars", "Venus", "Earth", "Jupiter"], answer: 2 },
-    { question: "What is 10 / 2?", options: ["3", "4", "5", "6"], answer: 2 },
-    { question: "What is the largest ocean?", options: ["Atlantic", "Indian", "Pacific", "Arctic"], answer: 2 },
-    { question: "What is 7 - 3?", options: ["5", "4", "3", "2"], answer: 1 },
+    { question: "What color is the sky?", options: ["Blue", "Green", "Red", "Yellow"], answer: 0 }
   ];
 
   useEffect(() => {  
@@ -84,7 +77,7 @@ const App = () => {
             <ul>
               <li><a className="tab active" id="Maths" onClick={() => handleTabClick('Maths')}>Maths</a></li>
               <li><a className="tab" id="SST" onClick={() => handleTabClick('SST')}>SST</a></li>
-              
+              <li><a className="tab" id="Science" onClick={() => handleTabClick('Science')}>Science</a></li>
             </ul>
             </div>
             <h2>{questions[currentQuestionIndex].question}</h2>
