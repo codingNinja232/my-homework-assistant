8e4e94a98dc9840c70a8c6dab2fcd666ac42bc12
codingNinja232 <coding.ninja232@gmail.com>
2024-12-16 19:19:03 +0530

    leaderboard changes done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [rank, setRank] = useState(0);
   const [isQuizComplete, setIsQuizComplete] = useState(false);
   const [activeTab, setActiveTab] = useState('');
   const [selectedOption, setSelectedOption] = useState(null);
@@ -24,14 +25,15 @@ const App = () => {
   const [timeLeft, setTimeLeft] = useState(120);
   const [timeElapsed, setTimeElapsed] = useState(0); // Percentage of time elapsed
   const [leaderboardData, setLeaderboardData] = useState([]);
-  
+  const [name, setName] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
 
   
 
   useEffect(() => {
     if (timeElapsed < 100 && !isQuizComplete) {
       const timer = setTimeout(() => {
-        setTimeElapsed((prev) => prev + (100 / 20)); // Increment percentage for 1 second
+        setTimeElapsed((prev) => prev + (100 / 120)); // Increment percentage for 1 second
       }, 1000);
       return () => clearTimeout(timer);
     } else if (timeElapsed >= 100) {
@@ -40,7 +42,7 @@ const App = () => {
   }, [timeElapsed, isQuizComplete]);
 
   const restartQuiz = () => {
-    setTimeLeft(20);
+    setTimeLeft(120);
     setScore(0);
     setCurrentQuestionIndex(0);
     setIsQuizComplete(false);
@@ -56,15 +58,6 @@ const App = () => {
         setError(null);
       })
       .catch(() => setError('Failed to load questions.'))
-      .finally(() => setIsLoading(false));
-
-      leaderboardClient.get(`exec?subjectName=${tab}`)
-      .then((response) => {
-        setLeaderboardData(response.data.leaderBoard);
-        setError(null);
-        console.log('leaderboardData : ' + leaderboardData);
-      })
-      .catch(() => setError('Failed to load leaderboard.'))
       .finally(() => setIsLoading(false));
   };
 
@@ -105,14 +98,32 @@ const App = () => {
     console.log('questions.length' + questions.length);
   };
 
+  const handlePublish = (e) => {
+    e.preventDefault(); // Prevent the page from refreshing
+    try {
+      leaderboardClient.get(`exec?subjectName=${activeTab}&name=${name}&score=${score}`)
+      .then((response) => {
+        setLeaderboardData(response.data.leaderBoard);
+        setRank(response.data.rank);
+        setError(null);
+        console.log('leaderboardData : ' + leaderboardData);
+      })
+      .catch(() => setError('Failed to load leaderboard.'))
+      .finally(() => setIsLoading(false));
+    } catch (error) {
+      setResponseMessage(`Error: ${error.response?.data?.message || error.message}`);
+    }
+    
+  };
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
   if (isStartScreen) {
     return (
       <div className="start-screen">
-        <h1>Welcome to the Quiz!</h1>
-        <p>Test your knowledge across various subjects. You would be presented with a question with 4 possible answers. If you select the correct answer you get 1 point and if you select a wrong answer 0.25 points would be deducted from your score. 
+        <h1>Welcome to the Homework Assistant!</h1>
+        <p>Test your knowledge across various subjects. You will get 2 min to answer as many questions as you like. You would be presented with a question with 4 possible answers. If you select the correct answer you get 1 point and if you select a wrong answer 0.25 points would be deducted from your score. 
           If you do not understand the question, you can skip it. Your score would not change if you skip the question. Select a category to start:</p>
         <div className="nav-bar">
           <button className="start-button" onClick={() => handleStartClick('Maths')}>Maths</button>
@@ -157,24 +168,45 @@ const App = () => {
         <div className="score-container">
           <h2>Quiz Complete!</h2>
           <p>Your Score: {score} out of {questions.length} questions. 
-            If you with to publish the scores to the leaderboard, please wait for our update. 
-            Following people have scored maximum score in the tests.
-            </p>
-            <div className="table">
-              <div className="table-row header">
-                <div className="table-cell">Name</div>
-                <div className="table-cell">Score</div>
-              </div>
-              
-                {leaderboardData.map((item, index) => (
-                  <div className="table-row" key={index}>
-                  <div className="table-cell">{item[0]}</div>
-                  <div className="table-cell">{item[1]}</div>
-                </div>
-              
-                ))}
-              
+            If you with to publish the scores to the leaderboard, please enter your name and click publish.
+          </p>
+          
+          <div style={{ padding: '20px' }}>
+          { rank === 0 &&
+          <div>
+          <form onSubmit={handlePublish}>
+            <div>
+              <label htmlFor="name">Name: </label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <button type="submit" style={{ marginLeft: '10px' }}>Publish</button>
             </div>
+            
+          </form>
+          </div>
+          }
+          {rank > 0 && <p> Your Rank is {rank}</p>}
+        </div>
+          {leaderboardData.length > 0 &&<div className="table">
+            <div className="table-row header">
+              <div className="table-cell">Name</div>
+              <div className="table-cell">Score</div>
+            </div>
+            
+              {leaderboardData.map((item, index) => (
+                <div className="table-row" key={index}>
+                <div className="table-cell">{item[0]}</div>
+                <div className="table-cell">{item[1]}</div>
+              </div>
+            
+              ))}
+            
+          </div>}
         </div>
       )}
     </div>
